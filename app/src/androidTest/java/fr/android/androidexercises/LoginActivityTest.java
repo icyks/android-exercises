19e57696100d55e420eca743002a145504db0e1b
icyks <kevin.keovilay@gmail.com>
2017-11-10 15:39:52 +0100

    ihm test

--- app/src/androidTest/java/fr/android/androidexercises/LoginActivityTest.java
+++ app/src/androidTest/java/fr/android/androidexercises/LoginActivityTest.java
@@ -1,12 +1,14 @@
 package fr.android.androidexercises;
 
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,5 +25,10 @@ public class LoginActivityTest {
     @Test
     public void shouldTryLoginAndSuccess() throws Exception {
         // TODO test login
+        Espresso.onView(ViewMatchers.withId(R.id.usernameEdit)).perform(ViewActions.typeText("abc@google.fr"));
+        Espresso.onView(ViewMatchers.withId(R.id.passwordEdit)).perform(ViewActions.typeText("password"));
+        ViewActions.closeSoftKeyboard();
+        Espresso.onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.loggedText)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 }
